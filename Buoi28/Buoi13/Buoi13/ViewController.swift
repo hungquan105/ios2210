4fa985e9c7bbf9e21f1f44f4af86139f70f40099
Quân <hungquan105@gmail.com>
2019-12-26 13:20:51 +0700

    commit

---
+++ Buoi28/Buoi13/Buoi13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buoi13
+//
+//  Created by Quân on 12/25/19.
+//  Copyright © 2019 QuanNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
